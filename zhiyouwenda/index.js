876d74ea4bbc9b21875fb5aadf91e0348fc0abdd
czq <572993469@qq.com>
2017-07-18 21:30:21 +0800

    a files

--- zhiyouwenda/index.js
+++ zhiyouwenda/index.js
@@ -1,264 +1,107 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
-var cookieParser = require('cookie-parser');
-
-
 var fs = require('fs');
 var multer = require('multer');
-
-
-var storage = multer.diskStorage({
-	
-	destination: 'wwwroot/uploads',
-	
-	filename: function(req, res, callback) {
-		var petname = req.cookies.petname;
-		callback(null, petname + '.jpg')
-	}
-});
-var upload = multer({ storage });
-
+var form = multer();
 var app = express();
 
 app.use(express.static('wwwroot'))
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.use(cookieParser());
+var users = [];
+var txt = [];
+app.post('/zc',form.array(), function(req, res) {
 
-app.post('/user/register', function(req, res) {
+	var isUser = false;
 
-	console.log('已进入注册请求')
+	var name = req.body.username;
+	var pwd = req.body.pwd;
 
-	fs.exists('users', function(exist) {
-		if(exist) {
+	if(name.length == 0) {
+		res.send('用户名不能为空');
+	} else if(pwd.length == 0) {
+		res.send('密码不能为空')
+	} else {
 
-			writeFile();
+		for(var i = 0; i < users.length; i++) {
+
+			if(users[i].name == name) {
+
+				isUser = true;
+
+				break;
+			}
+		}
+
+		if(isUser == true) {
+			res.send('用户名已经存在，重起一个名字吧！')
 		} else {
 
-			fs.mkdir('users', function(err) {
-				if(err) {
-					res.status(200).json({
-						message: '系统创建文件失败'
-					})
-				} else {
+			var newUser = {
+				name: name,
+				pwd: pwd
+			}
 
-					writeFile();
-				}
-			})
+			users.push(newUser);
+		}
+
+		var result = '注册成功啦!' + '<script>setTimeout(function(){location.href="login.html"},1000)</script>'
+		res.send(result);
+		console.log(users)
+	}
+	
+	
+	fs.exists('saveData',function(exist){
+		if(!exist){
+			fs.mkdirSync('saveData');
 		}
 	});
+	fs.appendFile('saveData/message.txt',JSON.stringify(users)+',',function(error){
+		if(error){
+			console.log('留言出错'+error)
+		}else{
+			console.log('留言成功')
+		}
+	})
+	res.send({
+		state: 'success',
+		info: '谢谢留言',
+	})
 
-	function writeFile() {
+})
 
-		var fileName = 'users/' + req.body.petname + '.txt';
-		fs.exists(fileName, function(exist) {
-			if(exist) {
+app.post('/login', function(req, res) {
 
-				res.status(200).json({ message: "用户名已经存在，请重新注册" })
-			} else {
+			var name = req.body.username;
+			var pwd = req.body.pwd;
 
-				fs.writeFile(fileName, JSON.stringify(req.body), function(err) {
-					if(err) {
-						res.status(200).json({
-							message: "写入文件操作失败"
-						});
+			var userIndex = null;
+
+			for(var i = 0; i < users.length; i++) {
+
+				if(name == users[i].name) {
+
+					//          userIndex = i;
+					if(pwd == users[i].pwd) {
+
+						var result = '登录成功啦!' + '<script>setTimeout(function(){location.href="index.html"},2000)</script>'
+						res.send(result);
 					} else {
-						res.status(200).json({
-							code: '1',
-							message: "注册成功"
-						});
+						res.send('密码错啦！！！')
 					}
-				});
-			}
-		});
-	}
-});
-
-app.post('/user/login', function(req, res) {
-
-	var fileName = 'users/' + req.body.petname + '.txt';
-	fs.exists(fileName, function(exist) {
-		if(exist) {
-
-			fs.readFile(fileName, function(err, data) {
-				if(err) {
-					res.status(200).json({
-						message: "文件读取失败"
-					});
 				} else {
-
-					var userData = JSON.parse(data);
-					if(userData.password == req.body.password) {
-
-						var expires = new Date();
-						expires.setMonth(expires.getMonth() + 1);
-						res.cookie('petname', req.body.petname, { expires })
-
-						res.status(200).json({
-							code: "1",
-							message: "登录成功"
-						});
-					} else {
-						res.status(200).json({
-							message: "密码错误"
-						});
-					}
+					res.send('用户不存在！快去注册一个！')
 				}
+			}
 			});
-		} else {
 
-			res.status(200).json({
-				message: "用户不存在，请先去注册"
-			})
-		}
-	})
-});
 
-app.post('/user/ask', function(req, res) {
 
-	console.log(req.cookies);
 
-	if(!req.cookies.petname) {
-		res.status(200).json({
-			message: "登录超时，请重新登录"
+		app.listen(3000, function() {
+			console.log('服务器已开启');
 		})
-		return;
-	}
-
-	fs.exists('questions', function(exist) {
-		if(exist) {
-
-			writeFile();
-		} else {
-
-			fs.mkdir('questions', function(err) {
-				if(err) {
-
-					res.status(200).json({
-						message: "文件创建失败"
-					})
-				} else {
-
-					writeFile();
-				}
-			})
-		}
-	})
-
-	function writeFile() {
-
-		var date = new Date();
-		var fileName = 'questions/' + date.getTime() + '.txt';
-
-		req.body.petname = req.cookies.petname
-		req.body.ip = req.ip
-		req.body.time = date
-
-		fs.writeFile(fileName, JSON.stringify(req.body), function(err) {
-			if(err) {
-				res.status(200).json({
-					message: "写入文件失败"
-				});
-			} else {
-				res.status(200).json({
-					code: "1",
-					message: "写入文件成功"
-				});
-			}
-		})
-	}
-})
-
-app.get('/question/all', function(req, res) {
-
-	fs.readdir('questions', function(err, files) {
-		if(err) {
-			res.status(200).json({ message: "文件夹读取失败" });
-		} else {
-
-			files = files.reverse();
-
-			var questions = [];
-
-			for(var i = 0; i < files.length; i++) {
-				var file = files[i];
-
-				var filePath = 'questions/' + file
-
-				var data = fs.readFileSync(filePath)
-
-				var object = JSON.parse(data)
-
-				questions.push(object);
-			}
-
-			res.status(200).json(questions);
-		}
-	})
-
-})
-
-app.post('/user/answer', function(req, res) {
-
-	if(!req.cookies.petname) {
-		res.status(200).json({ code: 0, message: '登录异常,请重新登录' });
-		return;
-	}
-
-	var question = req.cookies.question;
-	var filePath = 'questions/' + question + '.txt';
-	fs.readFile(filePath, function(err, data) {
-		if(!err) {
-			var object = JSON.parse(data);
-
-			if(!object.answers) {
-
-				object.answers = [];
-			}
-
-			req.body.content = req.body.content.replace(/</g, '&lt;');
-			req.body.content = req.body.content.replace(/>/g, '&gt;');
-
-			req.body.ip = req.ip;
-			req.body.question = question;
-			req.body.time = new Date();
-			req.body.petname = req.cookies.petname;
-
-			object.answers.push(req.body);
-			fs.writeFile(filePath, JSON.stringify(object), function(err) {
-				if(err) {
-
-					res.status(200).json({ message: '写入文件失败' });
-				} else {
-
-					res.status(200).json({ code: 1, message: '回复成功' });
-				}
-			});
-		}
-	});
-});
-
-app.post('/user/photo', upload.single('photo'), function(req, res) {
-
-	res.status(200).json({
-		code: "1",
-		message: "大头贴已上传"
-	})
-})
-
-/*******************退出登录*********************/
-app.get('/user/logout', function(req, res) {
-
-	res.clearCookie('petname')
-	res.status(200).json({
-		message: "退出成功"
-	});
-});
-
-app.listen(3000, function() {
-	console.log('服务器已开启');
-});
